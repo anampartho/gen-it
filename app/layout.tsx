133fcdabafdc7c73eee281b58b325162b119a863
Tashrik Anam <anampartho31@gmail.com>
2023-08-30 22:54:46 +0600

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Markdown to Carousel Post Generator',
+  description: 'Markdown to Carousel Post Generator',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      Markdown to Carousel Post Generator
+    </main>
+  )
+}
